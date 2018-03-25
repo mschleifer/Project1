129216f63f3e1008642869d31f9db95a0b79ec1c
Matthew Schleifer <matthewschleifer@gmail.com>
2018-03-25 09:55:25 -0500

    Initital commit; Panini example setup

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var panini = require('panini');
+
+gulp.task('panini', function () {
+    gulp.src('src/pages/**/*.html')
+        .pipe(panini({
+            root: 'src/pages/',
+            layouts: 'src/layouts/',
+            partials: 'src/partials/',
+            helpers: 'src/helpers/',
+            data: 'src/data/'
+        }))
+        .pipe(gulp.dest('dist'));
+});
