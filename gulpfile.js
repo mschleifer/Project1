4091b6afc54b56f11ab3dda68134fd0e42e57594
Matt Schleifer <matthewschleifer@gmail.com>
2018-03-25 22:17:59 -0500

    added browser-sync reloading; gulp functions for dist/assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
 var panini = require('panini');
+var browser = require('browser-sync');
 
-gulp.task('panini', pages);
+gulp.task('build', gulp.parallel(
+    pages,
+    function () {
+        return gulp.src('node_modules/material-components-web/dist/material-components-web.css')
+            .pipe(gulp.dest('dist/assets'));
+    },
+    function() {
+        return gulp.src('node_modules/material-components-web/dist/material-components-web.js')
+            .pipe(gulp.dest('dist/assets'));
+    }
+));
+
+gulp.task('default', gulp.series('build', server));
 
 function pages() {
     return gulp.src('src/pages/**/*.html')
@@ -20,4 +33,16 @@ function resetPages(done) {
     done();
 }
 
-gulp.watch('./src/{pages,layouts,partials,helpers,data}/**/*').on('all', gulp.series(resetPages, pages));
+function reloadBrowser(done) {
+    browser.reload();
+    done();
+}
+
+function server(done) {
+    browser.init({
+        server: 'dist', port: 8080, browser: 'chrome'
+    });
+    done();
+}
+
+gulp.watch('./src/{pages,layouts,partials,helpers,data}/**/*').on('all', gulp.series(resetPages, pages, reloadBrowser));
